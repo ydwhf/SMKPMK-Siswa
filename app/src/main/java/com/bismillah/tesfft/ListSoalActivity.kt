833f9ba59cc220f55851182cec5e9be2fe37a3df
yudawhf@gmail.com <yudawhf@gmail.com>
2025-05-22 21:21:10 +0700

    List Soal Layout

--- app/src/main/java/com/bismillah/tesfft/ListSoalActivity.kt
+++ app/src/main/java/com/bismillah/tesfft/ListSoalActivity.kt
@@ -1,8 +1,10 @@
 package com.bismillah.tesfft
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bismillah.tesfft.databinding.ActivityListSoalBinding
+import com.bismillah.tesfft.soal.SoalTema1Activity
 
 class ListSoalActivity : AppCompatActivity() {
     private lateinit var binding: ActivityListSoalBinding
@@ -11,5 +13,18 @@ class ListSoalActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityListSoalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnMulai.setOnClickListener()
+        {
+            startActivity(Intent(this, SoalTema1Activity::class.java))
+        }
+        binding.btnMulai2.setOnClickListener()
+        {
+
+        }
+        binding.btnMulai3.setOnClickListener()
+        {
+
+        }
     }
 }
